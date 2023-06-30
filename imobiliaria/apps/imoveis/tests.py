43cc2506c533838bff57fd630137dc6e9570c6e7
FelpsCorrea <felipecorrealopes1405@gmail.com>
2023-06-30 20:17:39 -0300

    Testes para Imoveis criados

--- imobiliaria/apps/imoveis/tests.py
+++ imobiliaria/apps/imoveis/tests.py
@@ -1,3 +1,135 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Imovel
 
-# Create your tests here.
+class ImovelApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        
+        # Cria 2 imoveis fictícios
+        self.imovel1 = Imovel.objects.create(
+            limite_hospedes=2,
+            aceita_animais=True,
+            valor_limpeza=100
+        )
+        self.imovel2 = Imovel.objects.create(
+            limite_hospedes=3,
+            aceita_animais=False,
+            valor_limpeza=150
+        )
+
+    '''
+        Teste que verifica se está funcionando a busca por um imóvel por seu id
+    '''
+    def test_get_single_imovel(self):
+
+        # Faz uma requisição GET para o endpoint
+        url = reverse('imovel-detail', kwargs={'pk': self.imovel1.id})
+        response = self.client.get(url)
+
+        # Testa se a resposta tem status code 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Testa se a resposta contém os dados corretos
+        self.assertEqual(response.data['id'], self.imovel1.id)
+        
+    '''
+        Teste que verifica se está funcionando a busca pelos imóveis que retorna a lista
+    '''
+    def test_get_list_imoveis(self):
+
+        # Faz uma requisição GET para o endpoint
+        url = reverse('imovel-list')
+        response = self.client.get(url)
+
+        # Testa se a resposta tem status code 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # Testa se dois imóveis são retornados
+        self.assertEqual(len(response.data), 2) 
+
+        # Verifica se os ids dos imóveis na resposta correspondem aos imóveis criados
+        response_ids = [imovel['id'] for imovel in response.data]
+        self.assertListEqual(response_ids, [self.imovel1.id, self.imovel2.id])
+        
+    '''
+        Teste que verifica se está funcionando a criação de imóveis
+    '''
+    def test_create_imovel(self):
+        
+        # Parâmetros fictícios
+        data = {
+            'limite_hospedes': 4,
+            'aceita_animais': False,
+            'valor_limpeza': 200
+        }
+        
+        # Faz uma requisição POST para o endpoint
+        url = reverse('imovel-list')
+        response = self.client.post(url, data)
+        
+        # Verifica se a resposta tem status code 201 (created)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # Verifica se um imóvel foi de fato criado
+        self.assertEqual(Imovel.objects.count(), 3)
+        
+        # Verifica se o imóvel criado tem os detalhes corretos
+        imovel = Imovel.objects.latest('id')
+        self.assertEqual(imovel.limite_hospedes, data['limite_hospedes'])
+        self.assertEqual(imovel.aceita_animais, data['aceita_animais'])
+        self.assertEqual(imovel.valor_limpeza, data['valor_limpeza'])
+        
+    '''
+        Teste que verifica se está funcionando o update de campos de um imóvel (PATCH pois é alteração parcial dos dados)
+    '''
+    def test_update_imovel(self):
+        
+        # Parâmetros fictícios
+        new_data = {
+            'limite_hospedes': 5,
+            'aceita_animais': True
+        }
+        
+        # Faz uma requisição PATCH para o endpoint
+        url = reverse('imovel-detail', kwargs={'pk': self.imovel1.id})
+        response = self.client.patch(url, new_data)
+        
+        # Verifica se a resposta tem status code 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # Recarrega o imovel1 com os dados atualizados do banco
+        self.imovel1.refresh_from_db()
+        
+        # Verifica se os dados do imovel1 foram atualizados corretamente
+        self.assertEqual(self.imovel1.limite_hospedes, new_data['limite_hospedes'])
+        self.assertEqual(self.imovel1.aceita_animais, new_data['aceita_animais'])
+        
+    '''
+        Teste que verifica se está funcionando o delete de um imóvel
+    '''
+    def test_delete_imovel(self):
+        
+        imovel = Imovel.objects.create(
+            limite_hospedes=3,
+            aceita_animais=False,
+            valor_limpeza=150
+        )
+        
+        # Faz uma requisição DELETE para o endpoint
+        url = reverse('imovel-detail', kwargs={'pk': imovel.id})
+        response = self.client.delete(url)
+        
+        # Verifica se a resposta tem status code 200 (no content)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+        # Recarrega o imovel com os dados atualizados do banco
+        imovel.refresh_from_db()
+        
+        # Verifica se o imovel sofreu um "Soft Delete"
+        self.assertEqual(imovel.ativo, False)
+        
+        # Verifica se agora possui 2 imóveis, considerando que 1 foi "deletado"
+        self.assertEqual(Imovel.objects.filter(ativo=True).count(), 2)
