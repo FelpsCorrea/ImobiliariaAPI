5e636e73a0cc795050e25d3efbea090e3f3188aa
FelpsCorrea <felipecorrealopes1405@gmail.com>
2023-06-30 22:33:13 -0300

    View do Anuncio e da PlataformaAnuncio criadas, testes unitários passados

--- imobiliaria/apps/anuncios/urls.py
+++ imobiliaria/apps/anuncios/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register(r'', views.AnuncioViewSet, basename='anuncio')
+router.register(r'plataformas', views.PlataformaAnuncioViewSet, basename='plataforma')
 
 urlpatterns = [
     path('', include(router.urls)),
--- imobiliaria/apps/anuncios/views.py
+++ imobiliaria/apps/anuncios/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Anuncio
+from .serializers import *
 
-# Create your views here.
+'''
+    CRUD de anúncio, sem incluir DELETE
+'''
+class AnuncioViewSet(viewsets.ModelViewSet):
+    
+    # Definição dos métodos disponíveis
+    http_method_names = ['get', 'post', 'patch']
+    
+    '''
+        Definição do queryset para considerar que os que tenham ativo=0 "não existem"
+    '''
+    def get_queryset(self):
+        return Anuncio.objects.filter(ativo=True)
+    
+    '''
+        Definição dos serializers para cada método da VIEW
+    '''
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return AnuncioCreateSerializer
+        return AnuncioSerializer  # caso contrário, o padrão é usado
+
+'''
+    GET das plataformas
+'''
+class PlataformaAnuncioViewSet(viewsets.ModelViewSet):
+    
+    http_method_names = ['get']
+    
+    '''
+        Definição do queryset para considerar que os que tenham ativo=0 "não existem"
+    '''
+    def get_queryset(self):
+        return PlataformaAnuncio.objects.filter(ativo=True)
+    
+    '''
+        Definição dos serializers para cada método da VIEW
+    '''
+    def get_serializer_class(self):
+        return PlataformaAnuncioSerializer
