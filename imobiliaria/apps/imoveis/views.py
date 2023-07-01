e92d63a3923cbbd6e655266c33672d03d67ef39d
FelpsCorrea <felipecorrealopes1405@gmail.com>
2023-06-30 22:20:42 -0300

    View do imovel criada, teste unitário passado

--- imobiliaria/apps/imoveis/views.py
+++ imobiliaria/apps/imoveis/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Imovel
+from .serializers import *
 
-# Create your views here.
+class ImovelViewSet(viewsets.ModelViewSet):
+    
+    # Definição dos métodos disponíveis    
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    
+    '''
+        Definição do queryset para considerar que os que tenham ativo=0 "não existem"
+    '''
+    def get_queryset(self):
+        return Imovel.objects.filter(ativo=True)
+    
+    '''
+        Definição dos serializers para cada método da VIEW
+    '''
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ImovelCreateSerializer
+        return ImovelSerializer  # caso contrário, o padrão é usado
